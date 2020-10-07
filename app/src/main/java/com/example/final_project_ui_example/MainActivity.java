e6db17fd6ba6a567274d0f6557526e8f6bdebc26
haerinHong <honghabbi9@gmail.com>
2020-10-07 14:08:27 +0900

    5 commit - new layout

--- app/src/main/java/com/example/final_project_ui_example/MainActivity.java
+++ app/src/main/java/com/example/final_project_ui_example/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     public final static int LOGIN_ACTIVITY = 100;
     public final static int START_ACTIVITY = 200 ;
+    public final static int REGISTER_ACTIVITY = 300;
+    public final static int START_ACTIVITY2 = 400 ;
     @Override
    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +21,18 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnLetslogin :
-                Intent login = new Intent(MainActivity.this, LoginActivity.class);
-                 startActivityForResult(login, LOGIN_ACTIVITY);
+//                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+//                 startActivityForResult(login, LOGIN_ACTIVITY);
+                Intent register = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivityForResult(register, REGISTER_ACTIVITY);
                 break;
             case R.id.btnStart:
-                Intent Start = new Intent(MainActivity.this, LoginActivity.class);
+                Intent Start = new Intent(MainActivity.this, AuthorizationActivity.class);
                 startActivityForResult(Start, START_ACTIVITY);
+                break;
+            case R.id.btn_if_login:
+                Intent iflogin = new Intent(MainActivity.this, IfLogin.class);
+                startActivityForResult(iflogin, START_ACTIVITY2);
                 break;
         }
     }
